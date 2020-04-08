408812ad721f30b205378d9df78d2b36cad24f83
terek97 <kurbanmagomedovr@gmail.com>
2020-04-09 01:55:02 +0300

    version 1.1 modified markers, info about volccanoes in popups

--- main.py
+++ main.py
@@ -1,16 +1,34 @@
 import folium
 import pandas
 
-map = folium.Map(location=[38.798510, -104.787883], zoom_start=5)    
+db = pandas.read_csv("volcanoes.txt")
+lat = list(db["LAT"])
+lon = list(db["LON"])
+name = list(db["NAME"])
+elev = list(db["ELEV"])
+
+def color_maker (elevation):
+    if elevation < 1500:
+        return "green"
+    elif elevation < 3000:
+        return "orange"
+    else:
+        return "red"
+
+
+map = folium.Map(location=[38.798510, -104.787883], zoom_start=5, tiles = "Stamen Terrain")    
 fg = folium.FeatureGroup(name="My Map")
 
-db = pandas.read_csv("volcanoes.txt")
-coordinates = db.iloc[:,-2:]
+html = """<h4>Volcano information:</h4>
+Volcano name: %s \n
+Height: %s m
+"""
 
-count = 0
-while count < len(coordinates):
-    fg.add_child(folium.Marker(location=[coordinates.LAT[count], coordinates.LON[count]], popup="ВУЛКАН", icon=folium.Icon(color='red')))
-    count = count + 1
+for lt, ln, na, el in zip(lat, lon, name, elev):
+    iframe = folium.IFrame(html=html % (na, str(el)), width=200, height=100)
+    fg.add_child(folium.CircleMarker(location=[lt, ln], popup=folium.Popup(iframe), 
+    fill_color=color_maker(el), fill_opacity = 0.9))
+
 map.add_child(fg)
 map.save("map1.html")
 
